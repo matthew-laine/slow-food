2ec0948caee785b698ea44274792f007fbd3c165
matthew-laine <matthew.i.laine@gmail.com>
2019-07-16 15:10:25 -0400

    initial commit: use spring initializr to put spring web starter, dev tools, jpa and h2 into build.gradle file

---
+++ src/main/java/com/kmstore/slowfood/SlowFoodApplication.java
@@ -0,0 +1,13 @@
+package com.kmstore.slowfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowFoodApplication.class, args);
+	}
+
+}
