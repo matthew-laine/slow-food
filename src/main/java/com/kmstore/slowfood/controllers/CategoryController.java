f2e1ebe5ffc5a994ca63e58bf005ed04147dcb08
KaiHennacy <kchennacy@gmail.com>
2019-07-25 08:28:54 -0400

    category controller retrieves all categories, a single category, and posts a single department to the repo

--- src/main/java/com/kmstore/slowfood/controllers/CategoryController.java
+++ src/main/java/com/kmstore/slowfood/controllers/CategoryController.java
@@ -1,5 +1,37 @@
 package com.kmstore.slowfood.controllers;
 
-public class CategoryController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.kmstore.slowfood.entities.Category;
+import com.kmstore.slowfood.repositories.CategoryRepository;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+	
+	@Autowired
+	CategoryRepository categoryRepo;
+	
+	@GetMapping("/categories")
+	public Iterable<Category> retrieveAllCategories(){
+		return categoryRepo.findAll();
+	}
+	
+	@GetMapping("/categories/{id}")
+	public Category retrieveCategory(@PathVariable Long id) {
+		return categoryRepo.findById(id).get();
+	}
+	
+	@PostMapping
+	public Iterable<Category> postSingleCategory(@PathVariable Category category){
+		categoryRepo.save(category);
+		return categoryRepo.findAll();
+	}
 }
