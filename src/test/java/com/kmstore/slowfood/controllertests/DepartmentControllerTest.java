287c7d09ae646930dfd47dd926683d96cc603899
matthew-laine <matthew.i.laine@gmail.com>
2019-07-25 07:41:06 -0400

    failing test for retrieving single department

--- src/test/java/com/kmstore/slowfood/controllertests/DepartmentControllerTest.java
+++ src/test/java/com/kmstore/slowfood/controllertests/DepartmentControllerTest.java
@@ -1,10 +1,12 @@
 package com.kmstore.slowfood.controllertests;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.contains;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -39,6 +41,12 @@ public class DepartmentControllerTest {
 		when(underTest.findAll()).thenReturn(Collections.singletonList(department1));
 		assertThat(underTest.retrieveAllDepartments(), contains(department1));
 		
+	}
+	
+	@Test
+	public void shouldReturnSingleDepartment() {
+		when(underTest.findById(1L)).thenReturn(Optional.of(department1));
+		assertThat(underTest.retrieveDepartment(1L), is(department1));
 	}
 
 }
