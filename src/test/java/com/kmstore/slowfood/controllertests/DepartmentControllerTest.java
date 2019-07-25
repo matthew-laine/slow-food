a6c6903a07051efc545952367959ea6af787d0dc
matthew-laine <matthew.i.laine@gmail.com>
2019-07-25 07:36:02 -0400

    failing test for retrieving all departments

---
+++ src/test/java/com/kmstore/slowfood/controllertests/DepartmentControllerTest.java
@@ -0,0 +1,44 @@
+package com.kmstore.slowfood.controllertests;
+
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.kmstore.slowfood.entities.Department;
+import com.kmstore.slowfood.repositories.DepartmentRepository;
+
+public class DepartmentControllerTest {
+	
+	@InjectMocks
+	private DepartmentController departmentController;
+	
+	@Mock
+	private DepartmentRepository underTest;
+	
+	@Mock
+	private Department department1;
+	
+	@Mock
+	private Department department2;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void shouldReturnListOfDepartments() {
+		when(underTest.findAll()).thenReturn(Collections.singletonList(department1));
+		assertThat(underTest.retrieveAllDepartments(), contains(department1));
+		
+	}
+
+}
