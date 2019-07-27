ce8153c82374c01eb102929a0cef0a5b94d0c25e
matthew-laine <matthew.i.laine@gmail.com>
2019-07-27 13:40:50 -0400

    try to render page

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,18 +1,21 @@
 import Api from "./api/Api";
 import Html from "./utils/html";
+import Components from "./utils/components/components";
 
-const testApi = new Api();
+const api = new Api();
 const html = new Html();
 
-testApi.getRequest(`http://localhost:8080/api/departments/1`, responseDept => {
-    console.log(responseDept)
-})
+const components = new Components();
+
+// Components().renderWholePage();
+
+Components().createSingleItem('foo', 'bar', 'http://lorempixel.com/400/200')
 
 
-const menuButton = Html().select(".site-nav__menu-button")
-const navUl = Html().select(".site-nav__list")
-console.log(menuButton.render())
-menuButton.click(event =>{
-    event.preventDefault()
-    navUl.toggleClass("hidden")
-})
+// const menuButton = Html().select(".site-nav__menu-button")
+// const navUl = Html().select(".site-nav__list")
+// console.log(menuButton.render())
+// menuButton.click(event =>{
+//     event.preventDefault()
+//     navUl.toggleClass("hidden")
+// })
--- front-end/js/utils/components/components.js
+++ front-end/js/utils/components/components.js
@@ -1,6 +1,5 @@
 import Html from "../html";
-import api from "../../api/Api";
-import Components from "../../../../../album-collection/front-end/js/utils/Components/Components";
+import Api from "../../api/Api";
 
 export default () => new Components();
 
@@ -16,16 +15,16 @@ class Components {
     }
 
     createSingleItem(name, info, imageUrl) {
-        return Html().create('div')
+        const item = Html().create('div')
             .addClass('item-box')
             .addChild(
                 Html().create('figure')
                     .addClass('item-figure')
-                    .addChild(
-                        Html().create('img')
-                            .addClass('item-figure__img')
-                            .addAttribute('src', imageUrl)
-                    )
+                    // .addChild(
+                    //     Html().create('img')
+                    //         .addClass('item-figure__img')
+                    //         .addAttribute('src', imageUrl)
+                    // )
             )
             .addChild(
                 Html().create('h1')
@@ -36,11 +35,31 @@ class Components {
                 Html().create('span')
                     .addClass('item-info-span')
                     .text(info)
-            )
+            );
+            console.log(item);
     }
 
     createItemGrid(itemCollection) {
-        
+        const itemGrid = Html().create('div')
+            .addClass('item-grid');
+        itemCollection.forEach(item => {
+            // console.log(item);
+            itemGrid.addChild(this.createSingleItem(item.name, item.categoryName, item.imageUrl));
+        });
+        return itemGrid;
     }
 
+    createProductGridFromEndPoint(endPoint) {
+        Api().getRequest(`http://localhost:8080/api/${endPoint}`, (responseCollection) => {
+            // console.log(responseCollection);
+            this.createItemGrid(responseCollection);
+        });
+    }
+
+    renderWholePage() {
+        const app = this.getAppContext();
+        const grid = this.createProductGridFromEndPoint('products');
+        console.log(grid);
+        // app.replace(grid);
+    }
 }
