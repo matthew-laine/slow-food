4697da48ca972f285bfc6f21d053fabbbd9b02ac
KaiHennacy <kchennacy@gmail.com>
2019-07-27 10:53:52 -0400

    expirementing with menu button hiding nav list

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,7 +1,14 @@
 import Api from "./api/Api";
+import Html from "./utils/html";
 
 const testApi = new Api();
+const html = new Html();
 
 testApi.getRequest(`http://localhost:8080/api/departments/1`, responseDept => {
     console.log(responseDept)
+})
+
+const menuButton = Html().select("nav-list__menu-button").click(event =>{
+    event.preventDefault()
+    const navUl = Html().select("site-nav__list").toggleClass()
 })
