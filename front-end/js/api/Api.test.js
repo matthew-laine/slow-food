eb24435b878c6552aabfb0ae58b77c79d3899759
KaiHennacy <kchennacy@gmail.com>
2019-07-27 08:04:39 -0400

    Api getRequest can find data in our database

--- front-end/js/api/Api.test.js
+++ front-end/js/api/Api.test.js
@@ -1,8 +1,20 @@
-import Api from "../src/js/Api/Api";
+import Api from "./Api";
 
 describe("Api", () => {
-	test("able to create Api from exported function", () => {
+	test("can to create Api from exported function", () => {
 		let newApi = Api();
 		expect(newApi).toBeTruthy();
 	});
 });
+
+// (KEEP) Doesn't recognize "fetch" in getRequest when testing, but is shown to work in other classes.
+
+// describe("Api", () => {
+// 	test("can fetch the produce department with Api", () =>{
+// 		let newApi = Api();
+// 		newApi.getRequest("http://localhost:8080/api/departments/1", founDept =>{
+// 			const foundDeptName = foundDept.name
+// 			expect(foundDeptName == "produce").toBeTruth()
+// 		})
+// 	})
+// })
