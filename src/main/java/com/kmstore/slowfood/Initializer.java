cf790ad5f86e35c3863704a299e46571224024d4
KaiHennacy <kchennacy@gmail.com>
2019-07-29 08:38:35 -0400

    Final entities, capitalizeEachWord is used for each string

--- src/main/java/com/kmstore/slowfood/Initializer.java
+++ src/main/java/com/kmstore/slowfood/Initializer.java
@@ -36,20 +36,8 @@ public class Initializer implements CommandLineRunner {
 		deptRepo.save(produce);
 		Department meat = new Department("meat");
 		deptRepo.save(meat);
-		Department seafood = new Department("seafood");
-		deptRepo.save(seafood);
 		Department deli = new Department("deli");
 		deptRepo.save(deli);
-		Department cheese = new Department("cheese");
-		deptRepo.save(cheese);
-		Department dairy = new Department("dairy");
-		deptRepo.save(dairy);
-		Department bakery = new Department("bakery");
-		deptRepo.save(bakery);
-		Department frozen = new Department("frozen");
-		deptRepo.save(frozen);
-		Department packaged = new Department("packaged");
-		deptRepo.save(packaged);
 	}
 
 	private void createCategories() {
@@ -71,26 +59,21 @@ public class Initializer implements CommandLineRunner {
 		Category dried = new Category("dried", deptRepo.findByName("meat"));
 		categoryRepo.save(dried);
 
-//		seafood
-		Category fish = new Category("fish", deptRepo.findByName("seafood"));
-		categoryRepo.save(fish);
-		Category roe = new Category("roe", deptRepo.findByName("seafood"));
-		categoryRepo.save(roe);
-		Category shellfish = new Category("shellfish", deptRepo.findByName("seafood"));
-		categoryRepo.save(shellfish);
-
 //      deli
-		Category deliBeef = new Category("deliBeef", deptRepo.findByName("deli"));
+		Category deliBeef = new Category("deli beef", deptRepo.findByName("deli"));
 		categoryRepo.save(deliBeef);
-
-//		Category deli = new Category("deli", deptRepo.findByName("seafood"));
-//		categoryRepo.save(deli);
+		Category deliHam = new Category("deli ham", deptRepo.findByName("deli"));
+		categoryRepo.save(deliHam);
+		Category deliTurkey = new Category("deli turkey", deptRepo.findByName("deli"));
+		categoryRepo.save(deliTurkey);
+		Category deliChicken = new Category("deli chicken", deptRepo.findByName("deli"));
+		categoryRepo.save(deliChicken);
 	}
 
 	private void createProducts() {
 //		PRODUCE
 //		fruits
-		Product strawberries = new Product("strawberries", "$3.99/lb", "https://driscolls.imgix.net/-/media/images/pages/finest-berries/strawberries/modal/long-stem.ashx", categoryRepo.findByName("fruits"));
+		Product strawberries = new Product("strawberries", "$3.99/lb", "https://hips.hearstapps.com/ghk.h-cdn.co/assets/cm/15/11/54fe493d07a7a-ghk-stainbuster-strawberry-mdn-98120767.jpg", categoryRepo.findByName("fruits"));
 		productRepo.save(strawberries);
 		Product tomatoes = new Product("tomatoes", "$0.69/lb", "http://pngimg.com/uploads/tomato/tomato_PNG12567.png", categoryRepo.findByName("fruits"));
 		productRepo.save(tomatoes);
@@ -127,17 +110,14 @@ public class Initializer implements CommandLineRunner {
 
 //		MEAT
 //		pork
-		Product blackForestHam = new Product("black forest ham", "$5.99/lb", "https://images-na.ssl-images-amazon.com/images/I/61F-oMTcApL._SX355_.jpg", categoryRepo.findByName("pork"));
-		productRepo.save(blackForestHam);
-		Product hamOffTheBone = new Product("ham off the bone", "$4.49/lb", "https://images-na.ssl-images-amazon.com/images/I/31vT6uGiZuL.jpg", categoryRepo.findByName("pork"));
-		productRepo.save(hamOffTheBone);
 		Product bacon = new Product("bacon", "$2.99/lb", "http://www.foodnutritiontable.com/_lib/img/prod/big/ontbijtspek.jpg", categoryRepo.findByName("pork"));
 		productRepo.save(bacon);
 		Product porkChops = new Product("pork chops", "$2.99/lb", "https://www.barfblog.com/wp-content/uploads/2011/06/pork_chop_raw.jpg", categoryRepo.findByName("pork"));
 		productRepo.save(porkChops);
 		Product porkloin = new Product("porkloin", "$2.99/lb", "https://image.shutterstock.com/image-photo/raw-pork-fillet-isolated-on-260nw-463142702.jpg", categoryRepo.findByName("pork"));
 		productRepo.save(porkloin);
-//beef
+		
+//		beef
 		Product burgerPatty = new Product("burger patty", "$1.99/lb", "https://s3.envato.com/files/248946677/38743_.jpg", categoryRepo.findByName("beef"));
 		productRepo.save(burgerPatty);
 		Product steak = new Product("steak", "$8.99/lb", "http://www.topratedsteakhouses.com/wp-content/uploads/2013/11/Raw-Rib-Eye-e1384445199924.jpg", categoryRepo.findByName("beef"));
@@ -146,7 +126,8 @@ public class Initializer implements CommandLineRunner {
 		productRepo.save(beefRibs);
 		Product roundSteak = new Product("round steak", "$3.99/lb", "https://cdn.shopify.com/s/files/1/1463/5240/products/top_round_steak_burned_large.png?v=1477316242", categoryRepo.findByName("beef"));
 		productRepo.save(roundSteak);
-//poultry
+		
+//		poultry
 		Product wholeTurkey = new Product("whole turkey", "$1.49/lb", "https://www.freepngimg.com/thumb/meat/33503-6-chicken-meat-transparent-background.png", categoryRepo.findByName("poultry"));
 		productRepo.save(wholeTurkey);
 		Product wholeChicken = new Product("whole chicken", "$0.99/lb", "http://www.m-tradegroup.com/wp-content/uploads/2017/04/22739276_s.jpg", categoryRepo.findByName("poultry"));
@@ -155,7 +136,46 @@ public class Initializer implements CommandLineRunner {
 		productRepo.save(wholeGoose);
 		Product wholeDuck = new Product("whole duck", "$7.99/lb", "https://www.trzcacak.rs/myfile/detail/31-318516_download-chicken-meat-png-images-background-broiler-chicken.png", categoryRepo.findByName("poultry"));
 		productRepo.save(wholeDuck);
-		Product goldenTurkey = new Product("golden turkey", "$3.99/lb", "http://www.miturkey.com/uploads/images/Content/RTC.png", categoryRepo.findByName("poultry"));
+		
+//		DELI
+//		deliBeef
+		Product londonBroil = new Product("london broil", "$9.99/lb", "https://www.wegmans.com/content/dam/wegmans/products/720/629720.jpg", categoryRepo.findByName("deli beef"));
+		productRepo.save(londonBroil);
+		Product slowRoastBeef = new Product("slow roast beef", "$8.99/lb", "https://chefsavvy.com/wp-content/uploads/Oven-Roasted-Beef-Brisket.jpg", categoryRepo.findByName("deli beef"));
+		productRepo.save(slowRoastBeef);
+		Product cajuanRoastBeef = new Product("cajuan roast beef", "$6.99/lb", "https://cutpcdnwimages.azureedge.net/images/products/95000/097684-75x75-B.jpg", categoryRepo.findByName("deli beef"));
+		productRepo.save(cajuanRoastBeef);
+		
+//		deliHam
+		Product goldenHam = new Product("golden ham", "$5.99/lb", "https://www.dorsogna.com.au/assets/Uploads/_resampled/CroppedFocusedImageWyIzMjAiLCIzNDAiLCJ4Iiw0Ml0/9694-Traditional-Golden-Roast-Leg-Ham.png", categoryRepo.findByName("deli ham"));
+		productRepo.save(goldenHam);
+		Product blackForestHam = new Product("black forest ham", "$5.99/lb", "https://images-na.ssl-images-amazon.com/images/I/61F-oMTcApL._SX355_.jpg", categoryRepo.findByName("deli ham"));
+		productRepo.save(blackForestHam);
+		Product hamOffTheBone = new Product("ham off the bone", "$4.49/lb", "https://images-na.ssl-images-amazon.com/images/I/31vT6uGiZuL.jpg", categoryRepo.findByName("deli ham"));
+		productRepo.save(hamOffTheBone);
+		Product mapleHam = new Product("maple ham", "$6.99/lb", "http://www.thegrocerygirls.com/media/catalog/product/cache/1/small_image/350x/9df78eab33525d08d6e5fb8d27136e95/4/G/4G01B.jpg.jpg", categoryRepo.findByName("deli ham"));
+		productRepo.save(mapleHam);
+		Product honeyHam = new Product("honey ham", "$5.99/lb", "https://doublejgulf.com/image/cache/catalog/deli-ham%20-%20448A-500x500.jpg", categoryRepo.findByName("deli ham"));
+		productRepo.save(honeyHam);
+		Product cajuanHam = new Product("cajuan ham", "$5.99/lb", "https://www.recipe4living.com/assets/itemimages/400/400/3/default_c8bb19b2fc82210b47adea72cf253ca3_Cajun-Style-Dry-Rubbed-Ham.jpg", categoryRepo.findByName("deli ham"));
+		productRepo.save(cajuanHam);
+		
+//		deliTurkey
+		Product goldenTurkey = new Product("golden turkey", "$5.99/lb", "http://www.miturkey.com/uploads/images/Content/RTC.png", categoryRepo.findByName("deli turkey"));
 		productRepo.save(goldenTurkey);
+		Product honeyTurkey = new Product("honey turkey", "$6.99/lb", "https://i5.walmartimages.com/asr/b2e06ce5-08ea-4e34-b66a-26a1742708ec_1.7223a60bb44a788f72fe480b6063a57c.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF", categoryRepo.findByName("deli turkey"));
+		productRepo.save(honeyTurkey);
+		Product blackForestTurkey = new Product("black forest turkey", "$6.99/lb", "https://images-na.ssl-images-amazon.com/images/I/814ctc2FuKL._SX355_.jpg", categoryRepo.findByName("deli turkey"));
+		productRepo.save(blackForestTurkey);
+		Product ovenClassicTurkey = new Product("oven classic turkey", "$3.99/lb", "https://s7d5.scene7.com/is/image/CentralMarket/001274365-1?$large$&hei=416&wid=416", categoryRepo.findByName("deli turkey"));
+		productRepo.save(ovenClassicTurkey);
+		Product mapleTurkey = new Product("maple turkey", "$3.99/lb", "https://images-na.ssl-images-amazon.com/images/I/3196k38sReL.jpg", categoryRepo.findByName("deli turkey"));
+		productRepo.save(mapleTurkey);
+		
+//		deliChicken
+		Product ovenChicken = new Product("oven chicken", "$4.99/lb", "https://i5.walmartimages.com/asr/1e6021ba-f993-4224-9e02-f1b7ed8f8f6c_1.00872be702396af8fd23c3ece0027c29.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF", categoryRepo.findByName("deli chicken"));
+		productRepo.save(ovenChicken);
+		Product buffaloChicken = new Product("buffalo chicken", "$3.99/lb", "https://cutpcdnwimages.azureedge.net/images/products/10000/013816-75x75-B.jpg", categoryRepo.findByName("deli chicken"));
+		productRepo.save(buffaloChicken);
 	}
 }
